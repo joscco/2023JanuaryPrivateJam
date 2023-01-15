9246d8574268d1a1e65dbb9e509a7b0d8b80bc94
Jonathan Schmitz <jomitz96@hotmail.com>
2023-01-15 22:07:44 +0100

    Flip Enemies if necessary

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -2,14 +2,29 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    [SerializeField]
-    private float movementSpeed = 1;
-    [SerializeField]
-    private Transform playerTransform;
+    [SerializeField] private float movementSpeed = 1;
+
+    private SpriteRenderer _renderer;
+    private GameObject _player;
+
+    private void Start()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+        _player = GameObject.Find("Player");
+    }
 
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, movementSpeed * Time.deltaTime);
+        var position = transform.position;
+        var playerPosition = _player.transform.position;
+        transform.position = Vector2.MoveTowards(position, playerPosition, movementSpeed * Time.deltaTime);
+        
+        float verticalDistance = playerPosition.x - position.x;
+        SwapFacingDirectionIfNeeded(verticalDistance);
     }
- 
+
+    private void SwapFacingDirectionIfNeeded(float verticalDistanceToPlayer)
+    {
+        _renderer.flipX = verticalDistanceToPlayer < 0;
+    }
 }
